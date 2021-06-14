d4af044dae84b850de260c913d04d890d71abf1d
BuildTools <unconfigured@null.spigotmc.org>
2021-06-15 02:45:31 +0300

    added sfml-triangle-4:1-1-x86_64.pkg.tar.zst package

--- x86_64/add-package.cpp
+++ x86_64/add-package.cpp
@@ -3,6 +3,7 @@
 #include <sys/sendfile.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <cstring>
 
 std::string& get_filename(const std::string& path)
 {
@@ -16,54 +17,64 @@ std::string& get_filename(const std::string& path)
 	return *new std::string;
 }
 
+void add_file_to_repo(const char* path)
+{
+	int input, output;
+	if ((input = open(path, O_RDONLY)) == -1)
+	{
+		std::cerr << "opening " << path << " failed : " << ::strerror(errno) << "\n";
+		exit(-1);
+	}
+	std::string& filename = get_filename(path);
+	
+	struct stat st{ };
+	if (::stat(filename.c_str(), &st) >= 0)
+	{
+		::remove(filename.c_str());
+	}
+	
+	if ((output = creat(filename.c_str(), 0644)) == -1)
+	{
+		std::cerr << "opening " << filename << " failed : " << ::strerror(errno) << "\n";
+		::close(input);
+		exit(-1);
+	}
+	
+	::fstat(input, &st);
+	
+	off_t offset = 0;
+	
+	if (::sendfile(output, input, &offset, st.st_size) == st.st_size)
+	{
+		std::cout << "copying file \033[32msuccessful\033[0m.\n";
+	}
+	else
+	{
+		std::cout << "copying file \033[31munsuccessful\033[0m.\n";
+	}
+	
+	::close(input);
+	::close(output);
+	
+	system("fish unconfigure.fish");
+	system(("repo-add xor-crypto-repo.db.tar.gz \'" + filename + "\'").c_str());
+	system("fish configure.fish");
+	system("git add *");
+	system(("git commit -m \"added " + filename + " package\"").c_str());
+}
+
 int main(int argc, char** argv)
 {
 	if (argc >= 2)
 	{
-		int input, output;
-		if ((input = open(argv[1], O_RDONLY)) == -1)
+		for (int i = 1; i < argc; ++i)
 		{
-			return -1;
+			add_file_to_repo(argv[i]);
 		}
-		std::string& filename = get_filename(argv[1]);
-		
-		struct stat st{ };
-		if (::stat(filename.c_str(), &st) >= 0)
-		{
-			::remove(filename.c_str());
-		}
-		
-		if ((output = creat(filename.c_str(), 0644)) == -1)
-		{
-			::close(input);
-			return -1;
-		}
-		
-		::fstat(input, &st);
-		
-		off_t offset = 0;
-		
-		if (::sendfile(output, input, &offset, st.st_size) == st.st_size)
-		{
-			std::cout << "copying file \033[32msuccessful\033[0m.\n";
-		}
-		else
-		{
-			std::cout << "copying file \033[31munsuccessful\033[0m.\n";
-		}
-		
-		system("unconfigure.fish");
-		system(("repo-add xor-crypto-repo.db.tar.gz \'" + filename + "\'").c_str());
-		system("configure.fish");
-		system("git add *");
-		system(("git commit -m \"added " + filename + " package\"").c_str());
 		system("git push");
-		
-		::close(input);
-		::close(output);
 	}
 	else
 	{
-		std::cout << argv[0] << " <path_to_package>\n";
+		std::cout << argv[0] << " <package...>\n";
 	}
 }
