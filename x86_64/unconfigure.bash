508223b97e329dccd850a1b0e4d97abfadfe3968
BuildTools <unconfigured@null.spigotmc.org>
2021-06-14 13:30:10 +0300

    added uncoufigure shell scripts

---
+++ x86_64/unconfigure.bash
@@ -0,0 +1,4 @@
+mv xor-crypto-repo.db xor-crypto-repo.db.tar.gz
+mv xor-crypto-repo.files xor-crypto-repo.files.tar.gz
+ln -s xor-crypto-repo.db.tar.gz xor-crypto-repo.db
+ln -s xor-crypto-repo.files.tar.gz xor-crypto-repo.files
