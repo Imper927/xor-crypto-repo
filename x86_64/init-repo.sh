0397f37ce8a0f3736ea8d9a6c8238e615b63172e
BuildTools <unconfigured@null.spigotmc.org>
2021-09-17 09:19:12 +0300

    init-repo.sh

--- x86_64/init-repo.sh
+++ x86_64/init-repo.sh
@@ -15,3 +15,4 @@
 ./add-package add ~/CLionProjects/neuro/neuro-*.pkg.tar.zst
 ./add-package add ~/CLionProjects/password-query/password-query-*.pkg.tar.zst
 ./add-package add ~/CLionProjects/system-actions-logger/system-action-logger-*.pkg.tar.zst
+./add-package add ~/CLionProjects/timing/timing-*.pkg.tar.zst
