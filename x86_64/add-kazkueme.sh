fee6135e2870553946e07d84b5db120fa62c169d
BuildTools <unconfigured@null.spigotmc.org>
2021-10-19 23:30:55 +0300

    add-kazkueme.sh

---
+++ x86_64/add-kazkueme.sh
@@ -0,0 +1,4 @@
+./add-package add ~/CLionProjects/kazkueme/kazkueme-*.pkg.tar.zst
+./add-package add ~/CLionProjects/kazkueme-comm/kazkueme-comm-*.pkg.tar.zst
+./add-package add ~/CLionProjects/kazkueme-client/kazkueme-client-*.pkg.tar.zst
+./add-package add ~/qtprojects/kazkueme-qt/kazkueme-qt-*.pkg.tar.zst
